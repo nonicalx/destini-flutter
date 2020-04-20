f9f87ee66748a8fe205ae6ccc8282dd446e6d459
oranye <n.oranye@genesystechhub.com>
2020-04-20 13:46:50 +0100

    destini set up and finished for now

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+//Story class.
+
+class Story {
+  //storyTitle to store the story text, choice1 to store the text for choice 1,choice2 to store the text for choice 2.
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+//Constructor for this class to be able to initialise the properties.
+
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
